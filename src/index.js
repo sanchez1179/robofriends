d66d0fe3d015bddfa707ee5d34f468ac91f622d1
sanchez1179 <icsanchez1179@gmail.com>
2019-05-07 06:13:09 -0700

    implemented thunk

--- src/index.js
+++ src/index.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {createStore, applyMiddleware, combineReducers} from 'redux';
 import {Provider} from 'react-redux';
-import {createStore} from 'redux';
 import './index.css';
 import  App from './containers/App';
+import thunkMiddleware from'redux-thunk';
 import 'tachyons';
+import {createLogger} from 'redux-logger';
 import registerServiceWorker from './registerServiceWorker';
+import {searchRobots, requestRobots} from "./reducers";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const logger =  createLogger();
+
+const rootReducer = combineReducers({searchRobots, requestRobots})
+const store =
+    createStore(rootReducer, applyMiddleware(thunkMiddleware, logger));
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App/>
+    </Provider>,
+    document.getElementById('root')
+);
 registerServiceWorker();
